138b8875f36fc7c62079a8a46e89ad6d21b1cc45
Russiancold <elykovalex@gmail.com>
2017-11-20 03:36:25 +0700

    gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,24 @@
 import org.jgrapht.Graph;
-import org.jgrapht.alg.interfaces.AStarAdmissibleHeuristic;
 import org.jgrapht.alg.shortestpath.ALTAdmissibleHeuristic;
 import org.jgrapht.alg.shortestpath.AStarShortestPath;
-import org.jgrapht.event.VertexSetListener;
-import org.jgrapht.graph.SimpleWeightedGraph;
-import org.jgrapht.graph.GraphWalk;
 
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
 public class Main {
+    private static final String RELATIVE_PATH = "/first_map.txt";
     public static void main(String [] args) throws IOException, ClassNotFoundException {
         // MapOfCity.createMap(10);
         //  MapOfCity.SaveToFile(mapOfCity, "/home/Eugene/Taxi/first_map.txt");
         // mapOfCity = MapOfCity.LoadFromFile(address);
 
-        String address = "/home/Eugene/Taxi/first_map.txt";
-        Graph mapOfCity = MapOfCity.LoadFromFile(address);
+        String filePath = new File("").getAbsolutePath() + RELATIVE_PATH;
+        System.out.println(filePath);
+        Graph mapOfCity = MapOfCity.LoadFromFile(filePath);
         System.out.println(mapOfCity);
         System.out.println("Shortest path by A* algorithm:");
         ALTAdmissibleHeuristic heuristic = new ALTAdmissibleHeuristic(mapOfCity, mapOfCity.vertexSet());
